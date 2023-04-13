ee712245c46af6c38d5d45a7a1bd9275e6a6f6e6
TaylanTorres09 <taylantorres@gmail.com>
2023-04-13 17:30:52 -0300

    Create DAO

---
+++ src/main/java/br/com/esig/models/dao/GenericDAO.java
@@ -0,0 +1,28 @@
+package br.com.esig.models.dao;
+
+import java.io.Serializable;
+
+import javax.persistence.EntityManager;
+
+public class GenericDAO<E> implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private static EntityManager entityManager = Connection.getEntityManager();
+	
+	public void create(E e) {
+		try {
+			
+			entityManager.getTransaction().begin();
+			
+			entityManager.persist(e);
+			
+			entityManager.getTransaction().commit();
+			
+		} catch (Exception e2) {
+			entityManager.getTransaction().rollback();
+			e2.printStackTrace();
+		}
+	}
+
+}
