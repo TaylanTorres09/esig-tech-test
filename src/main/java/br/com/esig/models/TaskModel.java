c558c590acb1da48ec5a4add0bb1ae4ec1b95406
TaylanTorres09 <taylantorres@gmail.com>
2023-04-13 16:30:00 -0300

    Create model to Tasks

---
+++ src/main/java/br/com/esig/models/TaskModel.java
@@ -0,0 +1,116 @@
+package br.com.esig.models;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tasks")
+public class TaskModel implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String title;
+	private String description;
+	private String responsable;
+	private String priority;
+	private String deadline;
+	private String situation;
+
+	public TaskModel() {
+	}
+	
+	public TaskModel(Long id, String title, String description, String responsable, String priority, String deadline,
+			String situation) {
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.responsable = responsable;
+		this.priority = priority;
+		this.deadline = deadline;
+		this.situation = situation;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getResponsable() {
+		return responsable;
+	}
+
+	public void setResponsable(String responsable) {
+		this.responsable = responsable;
+	}
+
+	public String getPriority() {
+		return priority;
+	}
+
+	public void setPriority(String priority) {
+		this.priority = priority;
+	}
+
+	public String getDeadline() {
+		return deadline;
+	}
+
+	public void setDeadline(String deadline) {
+		this.deadline = deadline;
+	}
+
+	public String getSituation() {
+		return situation;
+	}
+
+	public void setSituation(String situation) {
+		this.situation = situation;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TaskModel other = (TaskModel) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
