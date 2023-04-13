36a1e4a4e471d646704a9800de066f68316d9329
TaylanTorres09 <taylantorres@gmail.com>
2023-04-13 17:21:13 -0300

    Configure connection in application

---
+++ src/main/java/br/com/esig/models/dao/Connection.java
@@ -0,0 +1,15 @@
+package br.com.esig.models.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Connection {
+
+	private static EntityManagerFactory factory = Persistence.createEntityManagerFactory("management");
+	
+	public static EntityManager getEntityManager() {
+		return factory.createEntityManager();
+	}
+
+}
